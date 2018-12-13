9e2d20bee949d016c466d159a8037670b80d8b98
Jason Vance <=>
2018-12-13 14:04:07 -0600

    refactor signup form

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -6,49 +6,95 @@ import * as actions from '../../actions';
 
 export class Signup extends Component {
 
+    state = {
+        formError: ''
+    };
+
     componentDidMount() {
         this.props.setAuthError('');
-    }
+    } 
 
     onSubmit = (formProps) => {        
         this.props.signup(formProps, () => {
             //redirect user after authenticating
             this.props.history.push('/profile');
         });
-    };
+    };     
+
+    renderField = ({   
+        id,     
+        input,
+        label,
+        type,
+        meta: { touched, error, warning }
+      }) => {                   
+          return (           
+            <li>
+                <label htmlFor={id}>{label}</label>
+                <input {...input } placeholder={label} type={type} />  
+                {touched &&
+                    ((error && <span>{error}</span>) ||
+                      (warning && <span>{warning}</span>))}            
+            </li>  
+                                                               
+        )};
+                  
+                        
 
     render() {
 
         // handleSubmit is provided to props by redux form
-        const { handleSubmit } = this.props;       
+        const { handleSubmit } = this.props;   
+        console.log('PROPS', this.props);    
         return (          
             <div>
+                <div>{this.state.formError}</div>
                 {this.props.errorMessage && <div className="alert alert-danger">{this.props.errorMessage}</div>}
                 <form onSubmit={handleSubmit(this.onSubmit)}>
-                <fieldset>
-                    <label htmlFor="email">Email</label>
+                <ul className="flex-outer">
                     <Field 
                         name="email"
+                        id="email"
+                        label="Email"
                         type="text"
-                        component="input"
+                        component={this.renderField}
+                        validate={this.email}
                         autoComplete="none"
-                    />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="password">Password</label>
-                    <Field 
-                        name="password"
-                        type="password"
-                        component="input"
-                        autoComplete="none"
-                    />
-                </fieldset>                
-                <button>Sign up</button>
+                    />                                                       
+                        <Field 
+                            name="password"
+                            id="password"
+                            label="Password"
+                            type="password"
+                            component={this.renderField}
+                            autoComplete="none"
+                        />                                                    
+                    <li>
+                        <button className="btn">Sign up</button>
+                    </li>
+                </ul>             
+                
                 </form>
             </div>
         );
     }
 }
+
+const validate = values => {   
+    const errors = {};
+    if (!values.email) {
+      errors.email = 'Email is Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
+    if (!values.password) {
+        errors.password = 'Password is Required'
+    }
+    //console.log('ERRORS', errors);
+    return errors;
+}
+
+    
 
 const mapStateToProps = state => ({
     errorMessage: state.auth.errorMessage
@@ -56,5 +102,8 @@ const mapStateToProps = state => ({
 
 export default compose (
     connect(mapStateToProps, actions),
-    reduxForm({ form: 'signup' })
+    reduxForm({ 
+        form: 'signup',
+        validate  
+     })
 )(Signup);
--- src/components/test/profile.test.js
+++ src/components/test/profile.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import Profile from '../profile';
+import { Profile } from '../profile';
 
 describe('<Profile />', () => {
     it ('Renders without crashing', () => {
